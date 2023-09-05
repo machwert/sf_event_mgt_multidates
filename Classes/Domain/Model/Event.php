454f7040ce53604c042f29bac9a2ad6a67eb7011
V Golbig <vgolbig@yahoo.com>
2023-09-05 12:16:15 +0200

    adapted commentaries

--- Classes/Domain/Model/Event.php
+++ Classes/Domain/Model/Event.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the Extension "sf_event_mgt" for TYPO3 CMS.
+ * This file is part of the Extension "sf_event_mgt_multidates" for TYPO3 CMS which extends "sf_event_mgt".
  *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
